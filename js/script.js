4520bf996f5e61789271ea64283824a4ea3bdcaa
jojosoosoo <hyeoneegirl@naver.com>
2024-04-09 11:44:46 +0900

    연산자

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const car = ["현대", "기아", "벤츠", "BMW"];
+let a = 3,
+  b = "3",
+  c = 5,
+  d = 3;
 
-console.log(car[3]);
+console.log(!a == b); // true
+console.log(a === b); // false (a랑 b가 데이터 타입까지 같냐?)
+console.log(a != b); // false (a랑 b가 다르냐?)
+console.log(a !== b); // true (a랑 b가 데이터 타입까지 같냐?)
+console.log(a > c); // false
+console.log(a < c); // true
+console.log(a >= c); // false
+console.log(a <= c); // true
