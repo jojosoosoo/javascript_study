256a043e6ebc735a6f37e0508ab964712c0ae316
jojosoosoo <hyeoneegirl@naver.com>
2024-04-11 10:47:43 +0900

    for문

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-const scores = [88, 75, 95, 90, 78];
+// 이중 for문
+// 구구단
+// 2단
+let x = 2;
+let result;
 
-for (let score of scores) {
-  console.log(scores);
+for (let y = 1; y <= 9; y++) {
+  result = x * y;
+  console.log(result);
+}
+
+let resultAll;
+
+for (let x = 2; x <= 9; x++) {
+  for (let y = 1; y <= 9; y++) {
+    resultAll = x * y;
+    console.log(resultAll);
+  }
 }
